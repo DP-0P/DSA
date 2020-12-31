edc70a7f2162a340983ec90545e46a0ee25c1dd3
Deepak Pattnayak <60617347+DP-4419@users.noreply.github.com>
2020-12-31 07:50:17 +0530

    Add files via upload

---
+++ array1_ReverseOfArray.java
@@ -0,0 +1,15 @@
+public class array1_ReverseOfArray{
+    static <E> void reverseArray(E Array[]) {
+        int start, end;
+        E temp;
+        start = 0;
+        end = Array.length - 1;
+        while (start < end) {
+            temp = Array[start];
+            Array[start] = Array[end];
+            Array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
