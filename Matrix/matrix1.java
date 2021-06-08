d55eddca4ab344d944c92ba9cb0da1eaee467092
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-06-08 08:05:36 +0530

    Update matrix1.java

--- Matrix/matrix1.java
+++ Matrix/matrix1.java
@@ -1,8 +1,6 @@
 public static void spiralPrint(int matrix[][]) {
-
         int top = 0;
         int bottom = matrix.length - 1;
-
         int left = 0;
         int right = matrix[0].length - 1;
 
