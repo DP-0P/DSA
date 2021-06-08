a886432c13e96a56cf164fc9ab10fc89ac2f379a
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-06-08 06:05:29 +0530

    Delete Arrays directory

--- Arrays/array7_CyclicByOne.java
+++
@@ -1,9 +0,0 @@
-public class array7_CyclicByOne {
-    public static <E> E[] CyclicByOne(E[] array){
-        E last = array[array.length-1];
-        for(int i=array.length-1; i>0; i--)
-        array[i] = array[i-1];
-        array[0] = last;
-        return array;
-    }
-}
