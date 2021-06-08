87b068d4e7724ac0b0d308b66f43ba4063e8c947
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-06-08 08:01:44 +0530

    Add files via upload

---
+++ Matrix/matrix2.java
@@ -0,0 +1,17 @@
+public class matrix2 {
+
+    public static void maxOnesInARow(int matrix[][]) {
+
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            int cur = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1)
+                    cur++;
+                if (cur > max)
+                    max = cur;
+            }
+        }
+        System.out.println(max);
+    }
+}
