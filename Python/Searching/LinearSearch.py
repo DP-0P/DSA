a3ef5527b0dc72545394ea7a1afdfec30c9996ff
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-06-10 22:22:10 +0530

    Update LinearSearch.py

--- Python/Searching/LinearSearch.py
+++ Python/Searching/LinearSearch.py
@@ -4,17 +4,12 @@ def LinearSearch(array , search):
     for i in range(0,len(array)):
         if(i==search):
             index==i
-
+            
     if(i<0):
         print("Searched element is not available in the array")
     else:
         print("Searched element is available at index "+i)
         
-arr = [2,3,4,10,40]
-search = 10
-
-LinearSearch(arr,search)
-
 # Time Complexity -
 # Best Case - O(1)
 # Worst Case - O(n)
