46da0f2930b5b0418aa432c29cc2829d0e3aa2bd
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-06-14 12:55:06 +0530

    Add files via upload

---
+++ Java/Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int array[] = {3, 6, 2, 1, 4 };
+        SelectionSort(array);
+    }
+    public static void SelectionSort(int array[]){   
+        for(int i=0;i<array.length-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<array.length;j++)
+                if(array[j]<array[minIndex])
+                minIndex = j;
+            
+            if(minIndex!=i){
+              int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;  
+            }    
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+// Best Case - O(n²) when array is already sorted
+// Worst Case - O(n²) when array is sorted in descending order
+// Average Case - O(n²)
+// Space complexity - O(1)
