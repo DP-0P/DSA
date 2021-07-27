7204842559be29a6db230bdbb50e7ae7a578fccd
Deepak Pattnayak <deepakpattnayak2013@gmail.com>
2021-07-27 12:26:56 +0530

    Update reverse.cpp

--- C++/Arrays/reverse.cpp
+++ C++/Arrays/reverse.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 void reverse(int array[], int size)
 {
     int start = 0;
@@ -13,18 +10,4 @@ void reverse(int array[], int size)
         start++;
         end--;
     }
-}
-
-int main()
-{
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int size = sizeof(array) / sizeof(array[0]);
-    cout << size<<endl;
-    for (int i = 0; i < size; i++)
-        cout << array[i] << " ";
-    reverse(array, size);
-    cout<<endl;
-    for (int i = 0; i < size; i++)
-        cout << array[i] << " ";
-    return 0;
 }
